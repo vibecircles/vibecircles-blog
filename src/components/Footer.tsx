66f87312745472c4619b8bf8daea0b67de72a067
VibesCircles <vibecircless@gmail.com>
2025-08-15 21:00:59 +0200

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
           <Link
             href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
           >
-            Blog powered by wisp
+            Blog powered by VibeCircles
           </Link>
         </div>
         <Link href="/rss">
